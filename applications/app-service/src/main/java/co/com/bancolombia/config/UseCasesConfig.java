f7b4b90b735645b174ea6d32874b5f6a07d30e31
Germenty <dsmartincruz@gmail.com>
2025-08-26 00:00:17 -0500

    Add R2dbcConfig, WebFluxConfig, and UserUseCase and RolUseCase Beans

--- applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/bancolombia/config/UseCasesConfig.java
@@ -1,14 +1,28 @@
 package co.com.bancolombia.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import co.com.bancolombia.model.rol.gateways.RolRepository;
+import co.com.bancolombia.model.user.gateways.UserRepository;
+import co.com.bancolombia.usecase.rol.RolUseCase;
+import co.com.bancolombia.usecase.user.UserUseCase;
+
 @Configuration
-@ComponentScan(basePackages = "co.com.bancolombia.usecase",
-        includeFilters = {
-                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
-        },
-        useDefaultFilters = false)
+@ComponentScan(basePackages = "co.com.bancolombia.usecase", includeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+}, useDefaultFilters = false)
 public class UseCasesConfig {
+
+    @Bean
+    public UserUseCase userUseCase(UserRepository userRepository) {
+        return new UserUseCase(userRepository);
+    }
+
+    @Bean
+    public RolUseCase rolUseCase(RolRepository rolRepository) {
+        return new RolUseCase(rolRepository);
+    }
 }
